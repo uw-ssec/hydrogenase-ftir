fc878f2ca1cd96f9840f548ddc1565cb8510cb13
Anuradha Ramachandran <puju.anu@gmail.com>
2024-08-08 22:12:30 -0700

    feat : Added saving options to gaussian and lorentzian fit

--- src/hydrogenase_processing/peak_fit.py
+++ src/hydrogenase_processing/peak_fit.py
@@ -24,11 +24,12 @@ def lorentzian(x, *params):
     return y
 
 
-def peak_fit(fit_function, x_wavenumber, y_absorbance, peak_index, showplot = True): 
+def peak_fit(fit_function, x_wavenumber, y_absorbance, peak_index, sample_name, batch_id,showplot = True): 
 
     get_peak_wid = peak_widths(y_absorbance, peak_index, rel_height=1) 
     all_peak_wid = get_peak_wid[0]
     guess = []
+    
     for i, idx in enumerate(peak_index):
         peak_height = y_absorbance[idx]
         guess.append(peak_height)
@@ -36,7 +37,7 @@ def peak_fit(fit_function, x_wavenumber, y_absorbance, peak_index, showplot = Tr
         guess.append(peak_center)
         peak_wid = all_peak_wid[i]
         guess.append(peak_wid)
-
+    print(guess)
     if fit_function == 'Gaussian':
         objective_function = gaussian
     elif fit_function == 'Lorentzian':
@@ -46,23 +47,55 @@ def peak_fit(fit_function, x_wavenumber, y_absorbance, peak_index, showplot = Tr
 
     params, _ = curve_fit(objective_function, x_wavenumber, y_absorbance, guess)
 
+    fit_height = []
+    fit_center = []
+    fit_width = []
+    while i < len(peak_index):
+        print(i)
+        for j in range(i, len(peak_index),3):
+            print(j)
+
+
+
+
     # Calculate RMSE
     predicted_absorbance = objective_function(x_wavenumber, *params)
     residuals = y_absorbance - predicted_absorbance
     rmse = np.sqrt(np.mean(residuals**2))
+    fig, ax = plt.subplots(figsize=(10,5))
+    ax.plot(x_wavenumber,y_absorbance, label = 'baseline corrected data')
+    ax.plot(x_wavenumber, predicted_absorbance, label = f'{fit_function} curve fit')
+    if batch_id is not None:
+        ax.set_title(f'{sample_name} from batch_d {batch_id}')
+    else:
+        ax.set_title(f'{sample_name}')
+    ax.set_xlabel('wavenumber ($cm^{-1}$)')
+    ax.set_ylabel('absorbance')
+    ax.legend()
+
 
     if showplot:
-        plt.plot(x_wavenumber,y_absorbance, label = 'baseline corrected data')
-        plt.plot(x_wavenumber, predicted_absorbance, label = f'{fit_function} curve fit')
-        plt.legend()
-        
-    return params, rmse
+        plt.show()
+    else:
+        plt.close(fig)
+    return params, rmse, fig
+
 
 def gaussian_fit_prospecpy_objects(list_of_propspecpy_object, show_plots = False, save = True, verbose = True):
     """
-    Batched adaptation of second_deriv function.
+    Batched adaptation of gaussian_fit_baseline function.
     """
 
     for prospecpy_obj in list_of_propspecpy_object:
+            #print(f'processing plots for {prospecpy_obj.sample_name}')
             prospecpy_obj.gaussian_fit_baseline(show_plots, save, verbose)
+
+def lorentzian_fit_prospecpy_objects(list_of_propspecpy_object, show_plots = False, save = True, verbose = True):
+    """
+    Batched adaptation of lorentzian_fit_baseline function.
+    """
+
+    for prospecpy_obj in list_of_propspecpy_object:
+            #print(f'processing plots for {prospecpy_obj.sample_name}')
+            prospecpy_obj.lorentzian_fit_baseline(show_plots, save, verbose)
 
