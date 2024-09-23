4da5ee2a03813b2528b048d5e806401f9fa192ad
Anshul T. Tambay <anshul37@uw.edu>
2024-09-23 10:49:36 -0700

    Update postBuild.sh with tabulate package

--- .devcontainer/postBuild.sh
+++ .devcontainer/postBuild.sh
@@ -1,6 +1,6 @@
 # For writing commands that will be executed after the container is created
 
 # Install the Bruker Opus Reader
-python3 -m pip install matplotlib brukeropusreader openpyxl
+python3 -m pip install matplotlib brukeropusreader openpyxl tabulate
 python3 -m pip install -e .
 mkdir -p "/workspaces/hydrogenase-ftir/data"
